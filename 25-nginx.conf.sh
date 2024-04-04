cedfe9c6943ca38eb39db9ae84032031172b866c
Mitsuru Nakakawaji <mitsuru@procube.jp>
2024-04-04 04:47:34 +0000

    [[PRERELEASE]] Add X-Forwarded-For to log

--- 25-nginx.conf.sh
+++ 25-nginx.conf.sh
@@ -67,6 +67,7 @@ http {
 
     log_format json escape=json '{"time": "$time_iso8601",'
         '"vhost": "$host",'
+        '"xff": "$http_x_forwarded_for",'
         '"req": "${connection}-${connection_requests}",'
         '"user": "$oidc_user",'
         '"group": "$oidc_group",'
